80e897db1146d5525ac11e80107955f580b6fa72
jiro.kirameki <ec2-user@ip-172-31-34-226.us-east-2.compute.internal>
2018-06-20 00:43:44 +0000

    favbutton

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,11 +1,11 @@
 @if (Auth::user()->id != $user->id)
     @if (Auth::user()->is_favorites($micropost->id))
         {!! Form::open(['route' => ['user.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-xs"]) !!}
+            {!! Form::submit('❤', ['class' => "btn btn-outline-danger btn-xs"]) !!}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-info btn-xs"]) !!}
+            {!! Form::submit('❤', ['class' => "btn btn-danger btn-xs"]) !!}
         {!! Form::close() !!}
     @endif
 @endif
